723c3b502504c74365eaa0824c2e6ed8b8fb9207
Muhydeen O. Adelani <139554441+DataNaija@users.noreply.github.com>
2024-03-16 18:09:02 -0600

    Update EmailApp.java

--- emailapp/EmailApp.java
+++ emailapp/EmailApp.java
@@ -6,8 +6,5 @@ public class EmailApp {
         Email employ1 = new Email("Muhydeen", "Adelani");
         System.out.println(employ1.showInfo());
 
-       // employ1.setAlternateEmail("mo.adelani@gmail.com");
-        // System.out.println(employ1.getAlternateEmail());
-
     }
 }
